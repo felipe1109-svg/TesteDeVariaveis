e92636d262b4616fb7674ef746825d30a359bfe0
Felipe <Felipe@FELIPE-M6MJLFG>
2025-12-02 11:48:18 -0300

    Adicionar arquivos de projeto.

---
+++ TesteDeVariaveis/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Olá, Mundo!");
+Console.WriteLine("Teste de Variaveis");// Título do programa
+string nome = "Felipe";// Alterado para o meu nome
+int idade = 29;// Alterado para a minha idade
+float float1= 71.5f;// Alterado para o meu peso
+double double1= 1.75;// Alterado para a minha altura
+Console.WriteLine($"Meu nome é {nome}, minha idade é {idade} anos, meu peso é {float1} Kg e minha altura é {double1} metros");
